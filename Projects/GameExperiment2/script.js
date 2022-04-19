edf0b8dbb6d95b44df051c271094a3bd787d5e04
Tulloch022 <90489018+tulloch022@users.noreply.github.com>
2022-04-18 21:24:21 -0700

    added script to change grid number with arrow keys

--- Projects/GameExperiment2/script.js
+++ Projects/GameExperiment2/script.js
@@ -31,16 +31,38 @@ window.addEventListener("keydown", function (event) {
 //The Snake
 
 
-let currentBox = 55;
+let currentBox = [5,6];
+
 
 const snake = [document.querySelector("#box55")];
 
+
 const moveRight = () => {
-    currentBox -=1;
+    if (currentBox[1] < 10) {
+        currentBox[1] +=1;
+    }
     snake.push()
+    console.log(currentBox)
 }
 
 const moveLeft = () => {
-    snake.push(document.querySelector(""));
-    console.log(snake);
+    if (currentBox[1] > 1) {
+        currentBox[1] -=1;
+        console.log(currentBox);
+    }
+    snake.push(document.querySelector("#box56"));
+}
+
+const moveUp = () => {
+    if (currentBox[0] > 1) {
+        currentBox[0] -= 1;
+        console.log(currentBox);
+    }
+}
+
+const moveDown = () => {
+    if (currentBox[0] < 10) {
+        currentBox[0] += 1;
+        console.log(currentBox);
+    }
 }
